050274163c88cd8d3fb74b63b34a8e6283d8b854
Christian Possidonio <ch_ris@hotmail.com>
2019-06-16 16:48:27 -0300

    Init of API

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/**
+ * # API STAR WARS
+ * O **objetivo** desta API é gerar e gerenciar as informações dos planetas da franquia **STARWARS** e este arquivo é responsável por toda a estrutura da **API**.
+ */
+
+// O express é responsável por roterizar a API.
+const express = require('express')
+
+// Instâncio aqui o express
+const app = express()
+
+// O body-parser é responsável por gerenciar o app no modelo **REST**.
+const bodyParser = require('body-parser')
+
+// Aqui eu padronizo o body parser para sempre retornar **JSON**.
+app.use(bodyParser.json())
+
+//Responsável por entender os parametros via url.
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//Rota
+app.get('/', async(req, res) => {
+    res.send('Hello World')
+})
+
+//Ativa a API na porta 3000
+app.listen(3000)
